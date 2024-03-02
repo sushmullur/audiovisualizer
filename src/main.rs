ed302cd48c73704091a12ea8a7d1f122303a6b10
sushmullur <mullur.sushruth@gmail.com>
2024-03-02 09:12:52 -0800

    File picker implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use native_dialog::{FileDialog, MessageDialog, MessageType};
+
 fn main() {
-    println!("Hello, world!");
+    let path = FileDialog::new()
+        .set_location("~/")
+        .add_filter("Audio Files", &["wav", "mp3", "flac"])
+        .show_open_single_file()
+        .unwrap();
+    let path = match path {
+        Some(path) => path,
+        None => return,
+    };
+
+    let yes = MessageDialog::new()
+        .set_type(MessageType::Info)
+        .set_title("Success")
+        .set_text(&format!("You selected: {}", path.display()))
+        .show_confirm()
+        .unwrap();
+
+    if yes {
+        println!("User clicked yes {}", path.display());
+    } else {
+        println!("User clicked no");
+    }
 }
