be9d53077741ba84ac17bb489652eff5b4528d1c
sushmullur <mullur.sushruth@gmail.com>
2024-03-02 16:42:19 -0800

    5 second audio playback

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use native_dialog::{FileDialog, MessageDialog, MessageType};
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
 
 fn main() {
     let path = FileDialog::new()
@@ -11,16 +14,19 @@ fn main() {
         None => return,
     };
 
-    let yes = MessageDialog::new()
+    let _yes = MessageDialog::new()
         .set_type(MessageType::Info)
         .set_title("Success")
         .set_text(&format!("You selected: {}", path.display()))
         .show_confirm()
         .unwrap();
 
-    if yes {
-        println!("User clicked yes {}", path.display());
-    } else {
-        println!("User clicked no");
-    }
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let file = BufReader::new(File::open(path).unwrap());
+    let source = Decoder::new(file).unwrap();
+    let _ = stream_handle.play_raw(source.convert_samples());
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+
 }
